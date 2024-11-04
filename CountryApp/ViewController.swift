dfa9a029563ce6d55ba94b1ec71030967997e3a9
fiesematente <mosesfinimento@googlemail.com>
2024-11-04 15:57:30 +0100

    initail-commit for Country-App

--- CountryApp/ViewController.swift
+++ CountryApp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
+    let data = ["Deutsch", "Griechenland", "Frankreich", "Dänemark"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate{
+    
+}
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CountryTableViewCell
+        
+        cell.countryNameLabel.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
